dfe6af3956f6548d24bbc1a6dbc2ad2d996fc64e
marob <marob@smile.fr>
2015-04-29 18:16:40 +0200

    Add cordova-plugin-websqldatabase-initializer

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ var map = {
     'de.martinreinhardt.cordova.plugins.certificates' : 'cordova-plugin-certificates',
     'de.martinreinhardt.cordova.plugins.sqlite' : 'cordova-plugin-sqlite',
     'fr.smile.cordova.fileopener' : 'cordova-plugin-fileopener',
+    'org.smile.websqldatabase.initializer' : 'cordova-plugin-websqldatabase-initializer',
     'org.jboss.aerogear.cordova.push' : 'aerogear-cordova-push',
     'org.jboss.aerogear.cordova.oauth2' : 'aerogear-cordova-oauth2',
     'org.jboss.aerogear.cordova.geo' : 'aerogear-cordova-geo',
