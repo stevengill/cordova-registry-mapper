862bd6bc3155cc89c5dc8978844001701beda98d
Steve Gill <stevengill97@gmail.com>
2015-06-11 13:20:41 -0700

    removed weird indenting

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ var map = {
     'com.phonegap.plugins.barcodescanner' : 'phonegap-plugin-barcodescanner',
     'com.manifoldjs.hostedwebapp' : 'cordova-plugin-hostedwebapp',
     'com.initialxy.cordova.themeablebrowser' : 'cordova-plugin-themeablebrowser',
-	'gr.denton.photosphere' : 'cordova-plugin-panoramaviewer'
+    'gr.denton.photosphere' : 'cordova-plugin-panoramaviewer'
 }
 
 module.exports.oldToNew = map;
