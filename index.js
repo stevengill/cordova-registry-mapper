2e9b9bdb561cd139b21db4a6fc0a53f512e6d143
Josh Kasten <jkasten@gmail.com>
2015-07-09 13:27:54 -0700

    Update index.js
    Added mapping for OneSignal.

--- index.js
+++ index.js
@@ -100,7 +100,8 @@ var map = {
     'com.disusered.safe' : 'cordova-safe',
     'me.apla.cordova.app-preferences' : 'cordova-plugin-app-preferences',
     'com.konotor.cordova' : 'cordova-plugin-konotor',
-    'com.eface2face.iosrtc' : 'cordova-plugin-iosrtc'
+    'com.eface2face.iosrtc' : 'cordova-plugin-iosrtc',
+    'com.onesignal.plugins.onesignal' : 'onesignal-cordova-plugin'
 }
 
 module.exports.oldToNew = map;
